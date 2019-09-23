cefb75151e27ceb51cf9c3fac3e3db12c20123af
taoey <741494582@qq.com>
2019-09-23 13:02:51 +0800

    修改项目启动配置

--- src/myinit/InitConfig.go
+++ src/myinit/InitConfig.go
@@ -3,14 +3,12 @@ package myinit
 import (
 	"github.com/olebedev/config"
 	"os"
-	"strings"
 )
 
 var GCF *config.Config //global config
 
 func InitConf() {
 	pwd, _ := os.Getwd()
-	pwd = strings.Replace(pwd, "src\\myinit", "", -1) // 使用的替换方法，所以项目路径中不能包括init字符
-	configPath := pwd + "/src/config/application.yml"
+	configPath := pwd + "/configs/application.yml"
 	GCF, _ = config.ParseYamlFile(configPath)
 }
